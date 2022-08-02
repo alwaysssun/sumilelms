c6710da49a9b9c1dc5d70ce1791b066ccdd42db3
YU <yuhjij2@kakao.com>
2022-08-02 18:51:28 +0900

    sunmilelms 초기화 설정

---
+++ src/main/java/com/alwayssun/sumilelms/SumilelmsApplication.java
@@ -0,0 +1,13 @@
+package com.alwayssun.sumilelms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumilelmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SumilelmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alwayssun/sumilelms/SumilelmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alwayssun.sumilelms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumilelmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
